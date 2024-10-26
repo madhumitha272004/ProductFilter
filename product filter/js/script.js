7cc7314127ea60198e2fcd61977518f829c17991
Your Name <you@example.com>
2024-10-26 18:10:16 +0530

    div

--- product filter/js/script.js
+++ product filter/js/script.js
@@ -1,11 +1,10 @@
-const buttons=document.querySelectoroAll('.btn');
+const buttons=document.querySelectorAll('.btn');
 const boxes=document.querySelectorAll('.box');
 const searchBox=document.querySelector("#search");
 
 /* Search product by Textbox */
 searchBox.addEventListener('keyup' ,(e)=>{
 searchText=e.target.value.toLowerCase().trim();
-buttons.classList.remove('btn-clicked');
 boxes.forEach((box)=>{
     const data=box.dataset.item;
     if(data.includes(searchText)){
@@ -14,19 +13,29 @@ boxes.forEach((box)=>{
         box.style.display='none';
     }
 });
+buttons.forEach((button)=>{
+    button.classlist.remove('btn-clicked');
+});
+buttons[0].classList.add('btn-clicked');
 });
 buttons.forEach((button)=>{
     button.addEventListener('click',(e)=>{
         e.preventDefault();
         setActiveBtn(e);
-    });
+        const filter=e.target.dataset.filter;
+
+        boxes.forEach((box=>{
+            if(filter=='all'){
+                box.style.display="block";
+            }else{
+                const boxfilter=box.dataset.item;
+                if(btnfilter==boxfilter){
+                    box.style.display="block";
+                }else{
+                    box.style.display="none";
+                }
+            }
+
+    }));
 });
-
-function setActiveBtn(e){
-    buttons.forEach((button)=>{
-        button.classlist.remove('btn-clicked');
-    });
-    e.target.classList.add('btn-clicked');
-
-
-}
+});
